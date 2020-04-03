85ecdd3c923e53b101971f4ca5e156378b170fc5
Eder Christian <ederchristian92@gmail.com>
2020-04-03 01:25:52 -0300

    feat: add incidents lists as the map method

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg';
 
 export default function Profile() {
+  const [incidents, setIncidents] = useState([]);
+
+  const ongId = localStorage.getItem('ongId');
+  const ongName = localStorage.getItem('ongName');
+
+  useEffect(() => {
+    api.get('profile', {
+      headers: {
+        Authorization: ongId,
+      }
+    }).then(response => {
+      setIncidents(response.data);
+    })
+  }, [ongId]);
+
   return (
     <div className="profile-container">
       <header className="profile-container__header">
         <img className="profile-container__header__logo" src={logoImg} alt="Be the Hero"/>
-        <span className="profile-container__header__greeting">Bem-vinda, APAD!</span>
+        <span className="profile-container__header__greeting">Bem-vinda, {ongName}!</span>
 
         <Link className="profile-container__header__link" to="/incidents/new">Cadastrar novo caso</Link>
         <button className="profile-container__header__button" tyle="button">
@@ -22,65 +39,22 @@ export default function Profile() {
       <h1 className="profile-container__title">Casos cadastrados</h1>
 
       <ul className="profile-container__list">
-        <li className="profile-container__list__item">
-          <strong className="profile-container__list__item__title text-uppercase">Caso:</strong>
-          <p className="profile-container__list__item__description">Caso teste</p>
+        {incidents.map(incident => (
+          <li className="profile-container__list__item" key={incident.id}>
+            <strong className="profile-container__list__item__title text-uppercase">Caso:</strong>
+            <p className="profile-container__list__item__description">{incident.title}</p>
 
-          <strong className="profile-container__list__item__title text-uppercase">Descrição:</strong>
-          <p className="profile-container__list__item__description">Descrição teste</p>
+            <strong className="profile-container__list__item__title text-uppercase">Descrição:</strong>
+            <p className="profile-container__list__item__description">{incident.description}</p>
 
-          <strong className="profile-container__list__item__title text-uppercase">Valor:</strong>
-          <p className="profile-container__list__item__description">R$ 120,00</p>
+            <strong className="profile-container__list__item__title text-uppercase">Valor:</strong>
+            <p className="profile-container__list__item__description">{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}</p>
 
-          <button className="profile-container__list__item__button" type="button">
-            <FiTrash2 size={20} color="#A8A8B3" />
-          </button>
-        </li>
-
-        <li className="profile-container__list__item">
-          <strong className="profile-container__list__item__title text-uppercase">Caso:</strong>
-          <p className="profile-container__list__item__description">Caso teste</p>
-
-          <strong className="profile-container__list__item__title text-uppercase">Descrição:</strong>
-          <p className="profile-container__list__item__description">Descrição teste</p>
-
-          <strong className="profile-container__list__item__title text-uppercase">Valor:</strong>
-          <p className="profile-container__list__item__description">R$ 120,00</p>
-
-          <button className="profile-container__list__item__button" type="button">
-            <FiTrash2 size={20} color="#A8A8B3" />
-          </button>
-        </li>
-
-        <li className="profile-container__list__item">
-          <strong className="profile-container__list__item__title text-uppercase">Caso:</strong>
-          <p className="profile-container__list__item__description">Caso teste</p>
-
-          <strong className="profile-container__list__item__title text-uppercase">Descrição:</strong>
-          <p className="profile-container__list__item__description">Descrição teste</p>
-
-          <strong className="profile-container__list__item__title text-uppercase">Valor:</strong>
-          <p className="profile-container__list__item__description">R$ 120,00</p>
-
-          <button className="profile-container__list__item__button" type="button">
-            <FiTrash2 size={20} color="#A8A8B3" />
-          </button>
-        </li>
-
-        <li className="profile-container__list__item">
-          <strong className="profile-container__list__item__title text-uppercase">Caso:</strong>
-          <p className="profile-container__list__item__description">Caso teste</p>
-
-          <strong className="profile-container__list__item__title text-uppercase">Descrição:</strong>
-          <p className="profile-container__list__item__description">Descrição teste</p>
-
-          <strong className="profile-container__list__item__title text-uppercase">Valor:</strong>
-          <p className="profile-container__list__item__description">R$ 120,00</p>
-
-          <button className="profile-container__list__item__button" type="button">
-            <FiTrash2 size={20} color="#A8A8B3" />
-          </button>
-        </li>
+            <button className="profile-container__list__item__button" type="button">
+              <FiTrash2 size={20} color="#A8A8B3" />
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
