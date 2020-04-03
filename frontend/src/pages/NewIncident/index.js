02b24fa1fa15f2ea8decf8be086be01ba8ca1ae7
Eder Christian <ederchristian92@gmail.com>
2020-04-03 01:47:01 -0300

    feat: add a function to handle a New Incident

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg';
 
 export default function NewIncident() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('');
+
+  const history = useHistory();
+
+  const ongId = localStorage.getItem('ongId');
+
+  async function handleNewIncident(e) {
+    e.preventDefault();
+
+    const data = {
+      title,
+      description,
+      value,
+    };
+
+    try {
+      await api.post('incidents', data, {
+        headers: {
+          Authorization: ongId,
+        }
+      })
+
+      history.push('/profile');
+    } catch (err) {
+      alert('Erro ao cadastrar caso, tente novamente.');
+    }
+  }
+
   return (
     <div className="new-incident-container">
       <div className="new-incident-container__group">
@@ -21,11 +53,22 @@ export default function NewIncident() {
           </Link>
         </section>
 
-        <form className="new-incident-container__group__form">
-          <input placeholder="Título do caso" />
-          <textarea placeholder="Descrição" />
-          <input placeholder="WhatsApp" />
-          <input placeholder="Valor em Reais" />
+        <form className="new-incident-container__group__form" onSubmit={handleNewIncident}>
+          <input
+            placeholder="Título do caso"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
+          <textarea
+            placeholder="Descrição"
+            value={description}
+            onChange={e => setDescription(e.target.value)}
+          />
+          <input
+            placeholder="Valor em Reais"
+            value={value}
+            onChange={e => setValue(e.target.value)}
+          />
 
           <button className="new-incident-container__group__form__button" type="submit">Cadastrar</button>
         </form >
